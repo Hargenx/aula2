11ee2d5b9262215e18aa190dc675934328e6eccf
Raphael Mauricio Sanches de Jesus <55360548+Hargenx@users.noreply.github.com>
2023-08-29 15:06:32 -0300

    👍

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, TextInput, Button, TouchableHighlight, FlatList, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [text, setText] = useState('');
+  const [data, setData] = useState([
+    { id: '1', name: 'Item 1' },
+    { id: '2', name: 'Item 2' },
+    { id: '3', name: 'Item 3' },
+    { id: '4', name: 'Item 4' },
+  ]);
+
+  const renderItem = ({ item }) => (
+    <TouchableHighlight style={styles.item}>
+      <Text>{item.name}</Text>
+    </TouchableHighlight>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>Exemplo de Componentes</Text>
+      <Image
+        source={require('./assets/test.gif')}
+        style={styles.image}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Digite algo..."
+        value={text}
+        onChangeText={setText}
+      />
+      <Button title="Clique-me" onPress={() => alert('Botão clicado!')} />
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'cover',
+    marginBottom: 20,
+  },
+  input: {
+    width: 250,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 8,
+    marginBottom: 20,
+  },
+  item: {
+    padding: 10,
+    backgroundColor: '#f9f9f9',
+    borderBottomWidth: 1,
+    borderColor: '#ddd',
+  },
 });
+
+export default App;
